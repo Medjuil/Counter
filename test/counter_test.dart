8121ee40c9a19738bf3c6e7f7f30564bb98b4c18
Medjuil <mailuser0110@gmail.com>
2024-02-26 13:48:31 +0800

    adding setup and groups

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,25 +2,40 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:spyier/counter.dart';
 
 void main() {
-  // Given, When, Then
-  test('Given Counter classs, when instanstiated, then counter = 0', () {
-    // Arrange
-    Counter counter;
-    // Act
-    counter = Counter();
-    int count = counter.count;
-    // Assert
-    expect(count, 0);
-  });
+  group('Counter-class', () {
+    late Counter counter;
 
-  test(
-      'Given the Counter class is instantiated, when calling the incrementCount function, then count = 1',
-      () {
-    // arrange
-    Counter counter = Counter();
-    // act
-    counter.incrementCount();
-    // assert
-    expect(counter.count, 1);
+    setUp(() => counter = Counter());
+
+    // Given, When, Then
+    test('Given Counter classs, when instanstiated, then counter = 0', () {
+      // Arrange
+      // Act
+      counter = Counter();
+      int count = counter.count;
+      // Assert
+      expect(count, 0);
+    });
+
+    test(
+        'Given the Counter class is instantiated, when calling the incrementCount function, then count = 1',
+        () {
+      // arrange
+
+      // act
+      counter.incrementCount();
+      // assert
+      expect(counter.count, 1);
+    });
+
+    test(
+        'Given the Counter class is instantiated, when calling the incrementCount function, then count = -1',
+        () {
+      // arrange
+      // act
+      counter.decrementCount();
+      // assert
+      expect(counter.count, -1);
+    });
   });
 }
